eec93645efc10e93d2834bf670afd46a111b53a2
ulkshifer <kamza.0508@gmail.com>
2022-09-28 14:55:46 +0600

    fourth commit

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-fib1 = 1
-fib2 = 1
-
-n = input("Номер элемента ряда Фибоначчи: ")
-n = int(n)
-
-i = 0
-while i < n - 2:
-    fib_sum = fib1 + fib2
-    fib1 = fib2
-    fib2 = fib_sum
-    i = i + 1
-
-print("Значение этого элемента:", fib2)
+numbers = [1,2,3,4,9,16]
+b = []
+for i in numbers:
+    b.append(i**2)
+    print(b)
